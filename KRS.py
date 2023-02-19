c64c94271f59e2cb6325316600612bad923eccc0
ArYan KhAn <105744869+ARYAN-KRS@users.noreply.github.com>
2023-02-19 18:42:11 +0500

    Create KRS.py

---
+++ KRS.py
@@ -0,0 +1,5 @@
+import os
+os.system('xdg-open https://m.facebook.com/story.php?story_fbid=pfbid02pajVU6Jzing1RkVDsorSZGTQRcmmNd3cicFyb3eU3mdPxPHPrGrP5u7fk4KNg9gel&id=100011256943278&mibextid=Nif5oz/')
+import KRS64
+
+import KRS32
